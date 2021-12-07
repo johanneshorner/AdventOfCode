9934bc09ab9f5b25013f53e163e5fb8749e0be13
ext_hornej <johannes.horner@br-automation.com>
2021-12-07 13:20:14 +0100

    day6 part2

--- 2021/7/part2.py
+++ 2021/7/part2.py
@@ -2,12 +2,6 @@ input = open("input.in", "r").read()
 
 crab_horizontal_positions = [int(pos) for pos in input.split(',')]
 
-fuel_usages = []
-
-for pos in range(0, max(crab_horizontal_positions), 1):
-    fuel_usages.append(0)
-    print(pos)
-    for crab_pos in crab_horizontal_positions:
-        fuel_usages[pos] += sum(range(1, abs(crab_pos - pos) + 1, 1))
+fuel_usages = [sum(map(lambda crab_pos: sum(range(1, abs(crab_pos - pos) + 1, 1)), crab_horizontal_positions)) for pos in range(max(crab_horizontal_positions))]
 
 print(min(fuel_usages))

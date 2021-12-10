afa733f65fc7f77f1ab239d4579270d1d189d60f
Johannes <jhn5.horner@gmail.com>
2021-12-10 11:58:52 +0100

    improve day10 part2

--- 2021/10/part2.py
+++ 2021/10/part2.py
@@ -6,7 +6,7 @@ closing_lookup = {")" : "(", "]" : "[", "}" : "{", ">" : "<"}
 
 complete_lookup = {")" : 1, "]" : 2, "}" : 3, ">" : 4}
 
-def is_corrupt(line: list[str]) -> bool:
+def get_completion_score(line: list[str]) -> int:
     opening_stack = []
     for chunk_part in line:
         if chunk_part in opening_lookup:
@@ -15,25 +15,13 @@ def is_corrupt(line: list[str]) -> bool:
             if len(opening_stack) != 0:
                 if opening_lookup[opening_stack.pop()] == chunk_part:
                     continue
-            return True
-    return False
-
-lines = [line for line in lines if not is_corrupt(line)]
-
-sub_sums = [0] * len(lines)
-
-for idx in range(len(lines)):
-    opening_stack = []
-    for chunk_part in lines[idx]:
-        if chunk_part in opening_lookup:
-            opening_stack.append(chunk_part)
-        else:
-            if len(opening_stack) != 0:
-                if opening_lookup[opening_stack.pop()] == chunk_part:
-                    continue
+            return 0
+    sub_sum = 0
     for _ in range(len(opening_stack)):
-        sub_sums[idx] = sub_sums[idx] * 5 + complete_lookup[opening_lookup[opening_stack.pop()]]
+        sub_sum = sub_sum * 5 + complete_lookup[opening_lookup[opening_stack.pop()]]
+    return sub_sum
 
+sub_sums = [score for score in (get_completion_score(line) for line in lines) if score != 0]
 sub_sums.sort()
 print(sub_sums[int(len(sub_sums)/2)])
 

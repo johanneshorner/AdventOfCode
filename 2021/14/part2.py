d04a952311598f2a171618ae8d7e75c6f325edc7
Johannes <jhn5.horner@gmail.com>
2021-12-14 19:00:20 +0100

    restart day14 part2

--- 2021/14/part2.py
+++ 2021/14/part2.py
@@ -1,37 +1,18 @@
 from collections import defaultdict
 
-def parse_input(input_str: str):
-    template = input_str.split("\n\n")[0]
-    insertion_rule_lines = input_str.split("\n\n")[1].split("\n")
-
-    return (
-        template,
-        {left : right for left, right in (insertion_rule.split(" -> ") for insertion_rule in insertion_rule_lines)}
-    )
+STEPS = 40
 
 with open("input.in") as file:
-    template, insertion_rules = parse_input(file.read())
+    template, _, *insertion_rules = file.read().splitlines()
+    insertion_rules = dict(rule.split(" -> ") for rule in insertion_rules)
 
-def apply_step(polymers, insertion_rules):
-    polymers_new = polymers.copy()
-    for polymer in polymers:
+def apply_step(polymers: defaultdict, insertion_rules):
+    for polymer, count in polymers.copy().items():
         polymers_new[polymer[0] + insertion_rules[polymer[0 : 2]]] += polymers[polymer]
         polymers_new[polymer] -= polymers[polymer]
-    
     return polymers_new
 
 polymers = defaultdict(int, {template[i : i + 3] : 1 for i in range(len(template) - 2)})
-for i in range(1):
+
+for _ in range(STEPS):
     polymers = apply_step(polymers, insertion_rules)
-
-occurences = {char: 0 for char in set([char for polymer in polymers for char in polymer])}
-
-for char in occurences:
-    for polymer in polymers:
-        if char in polymer:
-            if char == template[0] or char == template[-1]:
-                occurences[char] += (polymer.count(char) / 2) + 1
-            else:
-                occurences[char] += polymer.count(char) / 2
-
-print(max(occurences.values()) - min(occurences.values()))

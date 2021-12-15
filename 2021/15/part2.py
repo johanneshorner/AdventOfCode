9e9daee67e5d2edcee484ada64b8ba811d3ca724
Johannes Horner <jhn5.horner@gmail.com>
2021-12-15 14:59:19 +0100

    day15 part2 not still not correct

--- 2021/15/part2.py
+++ 2021/15/part2.py
@@ -2,33 +2,36 @@ import math
 import heapq
 
 with open("input.in") as file:
-    risk_level_graph = [[int(risk_level) for risk_level in line.strip()] for line in file]
+    risk_level_grid = [[int(risk_level) for risk_level in line.strip()] for line in file]
 
-def dikstra(graph):
-    distances = {(x, y) : math.inf for y in range(len(graph)) for x in range(len(graph[0]))}
+def dikstra(grid, start):
+    distances = {(x, y) : math.inf for y in range(len(grid)) for x in range(len(grid[0]))}
 
-    distances[(0, 0)] = 0
-    heap = []
-    heapq.heappush(heap, (0, (0, 0)))
+    heap = [(0, start)]
 
-    while len(heap) > 0:
+    while heap:
         distance, (x, y) = heapq.heappop(heap)
         
-        if x < len(graph[0]) - 1:
-            if distance + graph[y][x + 1] < distances[(x + 1, y)]:
-                distances[(x + 1, y)] = distance + graph[y][x + 1]
-                heapq.heappush(heap, (distances[(x + 1, y)], (x + 1, y)))
-        if y < len(graph) - 1:
-            if distance + graph[y + 1][x] < distances[(x, y + 1)]:
-                distances[(x, y + 1)] = distance + graph[y + 1][x]
-                heapq.heappush(heap, (distances[(x, y + 1)], (x, y + 1)))
+        #already found shorter path
+        if distance > distances[(x, y)]:
+            continue
 
-    return distances[(len(graph) - 1, len(graph[0]) - 1)]
+        if x < len(grid[0]) - 1:
+            neighbor = (x + 1, y)
+            if distance + grid[y][x + 1] < distances[neighbor]:
+                distances[neighbor] = distance + grid[y][x + 1]
+                heapq.heappush(heap, (distances[neighbor], neighbor))
+        if y < len(grid) - 1:
+            neighbor = (x, y + 1)
+            if distance + grid[y + 1][x] < distances[neighbor]:
+                distances[neighbor] = distance + grid[y + 1][x]
+                heapq.heappush(heap, (distances[neighbor], neighbor))
 
-risk_level_graph = [
+    return distances[(len(grid) - 1, len(grid[0]) - 1)]
+
+risk_level_grid = [
     [x + j + i if x + j + i <= 9 else (x + j + i) - 9 for j in range(5) for x in y]
-    for i in range(5) for y in risk_level_graph
+    for i in range(5) for y in risk_level_grid
 ]
 
-risk_level_graph[0][0] = 0
-print(dikstra(risk_level_graph))
+print(dikstra(risk_level_grid, (0, 0)))
